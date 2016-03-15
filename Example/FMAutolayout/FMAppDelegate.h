25d4295915e5f80a413cc117422a311342e6bc91
franciscojma86 <franciscojma86@gmail.com>
2016-03-14 17:33:48 -0700

    Initial commit

---
+++ Example/FMAutolayout/FMAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  FMAppDelegate.h
+//  FMAutolayout
+//
+//  Created by franciscojma86 on 03/14/2016.
+//  Copyright (c) 2016 franciscojma86. All rights reserved.
+//
+
+@import UIKit;
+
+@interface FMAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Example/FMAutolayout/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  FMAutolayout
+//
+//  Created by franciscojma86 on 03/14/2016.
+//  Copyright (c) 2016 franciscojma86. All rights reserved.
+//
+
+@import UIKit;
+#import "FMAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([FMAppDelegate class]));
+    }
+}
